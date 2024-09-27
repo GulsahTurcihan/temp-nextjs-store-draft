eca8236bd45f87554305eb9938913d701f4f82ba
Gulsah Turcihan <gulsahturcihan@gmail.com>
2024-09-27 14:44:21 +0300

    next.config update

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
       }
     ],
   },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  },
 };
+
 
 export default nextConfig;
